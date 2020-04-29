1017edc7af8bd69c9762be06a528e8470125ac9f
Martino Mensio <martino.mensio@open.ac.uk>
2020-04-29 22:54:25 +0100

    prepare pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 def setup_package():
-    setup(name="spacy_dbpedia_spotlight", packages=find_packages())
+    setup(name="spacy_dbpedia_spotlight",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown')
 
 if __name__ == "__main__":
     setup_package()
