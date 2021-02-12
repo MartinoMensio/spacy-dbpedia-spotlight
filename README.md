53a2e7a10f9af2b7439e2d90f68d427456a70adb
Martino Mensio <martino.mensio@open.ac.uk>
2021-02-12 18:08:55 +0100

    refactor and update for spacy v3 documentation, overlapping entities, custom dbpedia spotlight instance. This refers to #1 and should fix it

--- README.md
+++ README.md
@@ -2,51 +2,90 @@
 
 This package acts as a Entity Recogniser and Linker using [DBpedia Spotlight](https://www.dbpedia-spotlight.org/), annotating SpaCy's Spans and adding them to the entities annotations.
 
-It can create a new [Language](https://spacy.io/api/language) object or be added on an existing one.
+It can be added to an existing spaCy [Language](https://spacy.io/api/language) object, or create a new one from an empty pipeline.
 
-It uses:
 
-- `DBPEDIA_ENT` as `ent_type`
-- the entity URI as `ent_kb_id`
+The results are put in `doc.ents`, overwriting existing entities in case of conflict depending on the `overwrite_ents` parameter.
+The spans produced have the following properties:
+
+- `span.label_ = 'DBPEDIA_ENT'`
+- `span.ent_kb_id_` containing the URI of the linked entity
+- `span._.dbpedia_raw_result` containing the raw json for the entity from DBpedia spotlight (`@URI`, `@support`, `@types`, `@surfaceForm`, `@offset`, `@similarityScore`, `@percentageOfSecondRank`)
 
 ## Usage
 
-Use on a blank new language
+## Installation
+
+This package works with SpaCy v3
+
+With pip: `pip install spacy-dbpedia-spotlight`
+From GitHub (after clone): `pip install .`
+
+### Instantiating the pipeline component
+
+With a blank new language
 
 ```python
 import spacy_dbpedia_spotlight
-
-# here a new blank model will be created
-nlp = spacy_dbpedia_spotlight.load('en')
-doc = nlp('The president of USA is calling Boris Johnson to decide what to do about coronavirus')
-print("Entities", [(ent.text, ent.label_, ent.kb_id_) for ent in doc.ents])
+# a new blank model will be created, with the language code provided in the parameter
+nlp = spacy_dbpedia_spotlight.create('en')
+# in this case, the pipeline will be only contain the EntityLinker
+print(nlp.pipe_names)
+# ['dbpedia_spotlight']
 ```
 
-Or use on top of an existing nlp object (added as last pipeline stage)
+On top of an existing nlp object (added as last pipeline stage by default)
 
 ```python
 import spacy
-import spacy_dbpedia_spotlight
-
-# use your model
+# this is any existing model
 nlp = spacy.load('en_core_web_lg')
-# pass nlp as parameter
-spacy_dbpedia_spotlight.load('en', nlp)
-doc = nlp('The president of USA is calling Boris Johnson to decide what to do about coronavirus')
-print("Entities", [(ent.text, ent.label_, ent.kb_id_) for ent in doc.ents])
+# add the pipeline stage
+nlp.add_pipe('dbpedia_spotlight')
+# see the pipeline, the added stage is at the end
+print(nlp.pipe_names)
+# ['tok2vec', 'tagger', 'parser', 'ner', 'attribute_ruler', 'lemmatizer', 'dbpedia_spotlight']
 ```
 
-Or if you want to just get the pipeline stage and place it where you want
+The pipeline stage can be added at any point of an existing pipeline (using the arguments `before`, `after`, `first` or `last`)
 
 ```python
 import spacy
+# this is any existing model
+nlp = spacy.load('en_core_web_lg')
+# add the pipeline stage
+nlp.add_pipe('dbpedia_spotlight', first=True)
+# see the pipeline, the added stage is at the beginning
+print(nlp.pipe_names)
+# ['dbpedia_spotlight', 'tok2vec', 'tagger', 'parser', 'ner', 'attribute_ruler', 'lemmatizer']
+```
+### Configuration parameters
 
-# any nlp you want
-nlp = spacy.blank('en')
-# create the pipe component, the dict argument is optional
-entity_annotator = nlp.create_pipe('annotate_dbpedia_spotlight', {'language_code':'en'})
-# add on your fancy pipeline with options like `first`
-nlp.add_pipe(entity_annotator, first=True)
+This component can be used with the following parameters:
+
+- `language_code`: to explicitly use a specific dbpedia language, because on default the value from `nlp.meta['lang']` is used
+- `dbpedia_rest_endpoint`: to use something different from `http://api.dbpedia-spotlight.org/{LANGUAGE_CODE}`, for example when using a local instance of DBpedia Spotlight. Don't set it if the default location is ok
+- `overwrite_ents`: to control how the overwriting of `doc.ents` is performed, because other components may have already written there (e.g., the `en_core_web_lg` model has a `ner` pipeline component which already sets some entities). The component tries to add the new ones from DBpedia, which can be successful if the entities do not overlap in terms of tokens. The cases are the following:
+  - no tokens overlap between the pre-exisiting `doc.ents` and the new entities: in this case `doc.ents` will contain both the previous entities and the new entities
+  - some tokens overlap and `overwrite_ents=True`: the previous value of `doc.ents` is saved in `doc.spans['ents_original']` and only the dbpedia entities will be saved in `doc.ents`
+  - some tokens overlap and `overwrite_ents=False`: the previous value of `doc.ents` is left untouched, and the dbpedia entiities can be found in `doc.spans['dbpedia_ents']`
+
+The configuration dict needs to be passed when instantiating the pipeline component
+
+```python
+import spacy
+nlp = spacy.load('en_core_web_lg')
+# instantiate Spanish EntityLinker on the English model
+nlp.add_pipe('dbpedia_spotlight', config={'language_code': 'es'})
+```
+
+### Using the model
+
+After having instantiated the component, you can use the spaCy API as usual
+
+```python
+doc = nlp('The president of USA is calling Boris Johnson to decide what to do about coronavirus')
+print("Entities", [(ent.text, ent.label_, ent.kb_id_) for ent in doc.ents])
 ```
 
 Output example:
@@ -56,13 +95,8 @@ Entities [('USA', 'DBPEDIA_ENT', 'http://dbpedia.org/resource/United_States'), (
 
 
 ## Known issues
-
-### Overlap of entities
-
-Spacy v2 does not allow entities to overlap, so the current implementation removes the entities computed by other models and places the ones from DBpedia. Future versions using spaCy v3 won't have this problem.
-
 ### DBpedia spotlight is denying my requests
 
-After a few requests to DBpedia spotlight, the public web service will reply with some HTTP codes.
+After a few requests to DBpedia spotlight, the public web service will reply with some bad HTTP codes.
 
 The solution is to use a local DBpedia instance (TODO documentation will come soon).
--- spacy_dbpedia_spotlight/entity_linker.py
+++ spacy_dbpedia_spotlight/entity_linker.py
@@ -1,59 +1,117 @@
 import spacy
 import requests
+from spacy.language import Language
 
 from spacy.tokens import Span
 
-supported_languages = ['en', 'de', 'es', 'fr', 'it', 'nl', 'pt', 'ru']
+# span extension attribute for raw json
+Span.set_extension("dbpedia_raw_result", default=None)
 
-class DbpediaLinker(object):
-    base_url = 'http://api.dbpedia-spotlight.org/'
+@Language.factory('dbpedia_spotlight', default_config={
+    'language_code': None,
+    'dbpedia_rest_endpoint': None,
+    'overwrite_ents': True,
+    'debug': False
+})
+def dbpedia_spotlight_factory(nlp, name, language_code, dbpedia_rest_endpoint, overwrite_ents, debug):
+    '''Factory of the pipeline stage `dbpedia_spotlight`.
+    Parameters:
+    - `language_code`: which language to use for entity linking. Possible values are listed in EntityLinker.supported_languages. If the parameter is left as None, the language code is matched with the nlp object currently used.
+    - `dbpedia_rest_endpoint`: this needs to be configured if you want to use a different REST endpoint from the default `EntityLinker.base_url`. Example: `http://localhost:2222/rest` for a localhost server
+    - `overwrite_ents`: if set to False, it won't overwrite `doc.ents` in cases of overlapping spans with current entities, and only produce the results in `doc.spans['dbpedia_ents']. If it is True, it will move the entities from doc.ents into `doc.spans['ents_original']`
+    - `debug`: prints several debug information to stdout
+    '''
+    if debug:
+        print('dbpedia_spotlight_factory:', nlp, 'language_code', language_code, 'dbpedia_rest_endpoint', dbpedia_rest_endpoint, 'overwrite_ents', overwrite_ents)
+    # take the language code from the nlp object
+    nlp_lang_code = nlp.meta['lang']
+    if debug:
+        print('nlp.meta["lang"]=', nlp_lang_code)
+    # language_code can override the language code from the nlp object
+    if not language_code:
+        language_code = nlp_lang_code
+    return EntityLinker(language_code, dbpedia_rest_endpoint, overwrite_ents, debug)
 
-    def __init__(self, language_code, nlp=None, dbpedia_rest_api=None):
-        if language_code not in supported_languages:
-            raise ValueError(f'Linker not available in {language_code}. Choose one of {supported_languages}')
+
+class EntityLinker(object):
+    '''This class manages the querying of DBpedia and attaches the found entities to the document'''
+    # default location of the service
+    base_url = 'http://api.dbpedia-spotlight.org'
+    # list of supported languages
+    supported_languages = ['en', 'de', 'es', 'fr', 'it', 'nl', 'pt', 'ru']
+
+    def __init__(self, language_code='en', dbpedia_rest_endpoint=None, overwrite_ents=True, debug=False):
+        # constructor of the pipeline stage
+        if language_code not in self.supported_languages:
+            raise ValueError(f'Linker not available in {language_code}. Choose one of {self.supported_languages}')
         self.language_code = language_code
-        if dbpedia_rest_api:
-            self.api_endpoint = dbpedia_rest_api # 'http://localhost:2222/rest'
+        self.overwrite_ents = overwrite_ents
+        self.debug = debug
+        if dbpedia_rest_endpoint:
+            # override the default endpoint, e.g., 'http://localhost:2222/rest'
+            self.api_endpoint = dbpedia_rest_endpoint
+            if debug:
+                print('api_endpoint has been manually set to', self.api_endpoint)
         else:
-            self.api_endpoint = f'{self.base_url}{self.language_code}'
+            # use the default endpoint for the language selected
+            self.api_endpoint = f'{self.base_url}/{self.language_code}'
+            if debug:
+                print('api_endpoint has been built as', self.api_endpoint)
 
-        if not nlp:
-            nlp = spacy.blank(language_code)
-        
-        if 'ner' not in nlp.pipe_names:
-            ner = nlp.create_pipe('ner')
-            nlp.add_pipe(ner)
-            nlp.begin_training()
-        
-        nlp.entity.add_label('DBPEDIA_ENT')
-
-        def annotate_dbpedia_spotlight(doc):
-            self._annotate_dbpedia_spotlight(doc)
-            return doc
-        
-        nlp.add_pipe(annotate_dbpedia_spotlight)
-        self.nlp = nlp
-
-
-    def _annotate_dbpedia_spotlight(self, doc, debug=True):
-        print('running remote', self.api_endpoint)
-
-        response = requests.get(f'{self.api_endpoint}/annotate', headers={'accept': 'application/json'}, params={'text': doc.text})
-        response.raise_for_status()
-        data = response.json()
-        if debug:
-            print(data)
-
-        ents_data = []
-        for ent in data.get('Resources', []):
-            start_ch = int(ent['@offset'])
-            end_ch = int(start_ch + len(ent['@surfaceForm']))
-            ent_kb_id = ent['@URI']
-            # TODO look at '@types' and choose most relevant?
-            span = doc.char_span(start_ch, end_ch, 'DBPEDIA_ENT', ent_kb_id)
-            ents_data.append(span)
-        
-        # TODO manage overlapping spans with spacy v3
-        # doc.ents = list(doc.ents) + ents_data
-        doc.ents = ents_data
+    def __call__(self, doc):
+        # called in the pipeline
+        annotate_dbpedia_spotlight(doc, self.api_endpoint, self.overwrite_ents, self.debug)
         return doc
+
+def create(language_code, nlp=None):
+    '''Creates an instance of a Language with the DBpedia EntityLinker pipeline stage.
+    If the parameter `nlp` is None, it will return a blank language with the EntityLinker.
+    If the parameter `nlp` is an existing Language, it simply adds the EntityLinker pipeline stage (equivalent to `nlp.add`)
+    '''
+    if not nlp:
+        nlp = spacy.blank(language_code)
+    nlp.add_pipe('dbpedia_spotlight')
+    return nlp
+
+
+def annotate_dbpedia_spotlight(doc, api_endpoint, overwrite_ents, debug=True):
+    if debug:
+        print('running remote', api_endpoint)
+
+    response = requests.get(f'{api_endpoint}/annotate', headers={'accept': 'application/json'}, params={'text': doc.text})
+    response.raise_for_status()
+    data = response.json()
+    if debug:
+        print(data)
+
+    ents_data = []
+    for ent in data.get('Resources', []):
+        start_ch = int(ent['@offset'])
+        end_ch = int(start_ch + len(ent['@surfaceForm']))
+        ent_kb_id = ent['@URI']
+        # TODO look at '@types' and choose most relevant?
+        span = doc.char_span(start_ch, end_ch, 'DBPEDIA_ENT', ent_kb_id)
+        span._.dbpedia_raw_result = ent
+        ents_data.append(span)
+    
+    # try to add results to doc.ents
+    try:
+        doc.ents = list(doc.ents) + ents_data
+        if debug:
+            print('the entities are in doc.ents')
+    except Exception as e:
+        if debug:
+            print(e)
+        if overwrite_ents:
+            # overwrite ok
+            doc.spans['ents_original'] = doc.ents
+            doc.ents = ents_data
+            if debug:
+                print('doc.ents has been overwritten. The original entities are in doc.spans["ents_original"]')
+        else:
+            # don't overwrite
+            if debug:
+                print('doc.ents not overwritten. You can find the dbpedia ents in doc.spans["dbpedia_ents"]')
+    # doc.spans['dbpedia_raw'] = data
+    doc.spans['dbpedia_ents'] = ents_data
+    return doc
